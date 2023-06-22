5192a4423f3be8b36c6fba1005e857793b4ec943
agismandala007 <agixsatria@gmail.com>
2023-06-22 13:27:26 +0700

    view default

--- application/views/view_registrasi.php
+++ application/views/view_registrasi.php
@@ -5,19 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
     <title>Register</title>
 </head>
 
 <body>
 
-    <header class="px-3 py-2 border-bottom mb-3">
-        <div class="container d-flex flex-wrap justify-content-center">
+    <header>
+        <div>
             <a href="<?php echo base_url('home') ?>">Website</a>
-            <div class="text-end">
-                <a class="btn btn-primary" href="<?php echo base_url('login/index'); ?>">Login</a>
-                <a class="btn btn-light text-dark me-2" href="<?php echo base_url('registrasi/index'); ?>">Register</a>
+            <div>
+                <a href="<?php echo base_url('login/index'); ?>">Login</a>
+                <a href="<?php echo base_url('registrasi/index'); ?>">Register</a>
             </div>
         </div>
     </header>
